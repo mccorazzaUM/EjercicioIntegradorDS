a340f5584c981a1d5cc443e0a8e1ee3a0300940b
mccorazzaUM <mc.corazza@alumno.um.edu.ar>
2026-08-26 10:04:38 -0300

    importar routers

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from app.modules.producto.routers import router as producto_router
+from app.modules.categoria.routers import router as categoria_router
 
 def create_app() -> FastAPI:
     app = FastAPI(
@@ -6,6 +8,9 @@ def create_app() -> FastAPI:
         description="Corazza",
         version="1.0.0"
     )
+
+    app.include_router(producto_router)
+    app.include_router(categoria_router)
 
     return app
 
